eb28d43152dfc3bbc65c42cc3c90d7dbc8cc5a01
idoneaseg <geral@centronegocios.pt>
2025-10-10 22:37:15 +0100

    Update serverRoutes.js
    fix: add .js extensions and correct route variable names
    
    
    Signed-off-by: idoneaseg <geral@centronegocios.pt>

--- server/routes/serverRoutes.js
+++ server/routes/serverRoutes.js
@@ -1,32 +1,32 @@
 import express from "express";
 const router = express.Router();
 
-import LeadRoute from "./leadRoutes";
-import ContactRoute from "./contactRoutes"
-import ClaimRoute from './claimRoutes'
-import NoteRoute from "./noteRoutes"
-import CallRoute from "./callRoutes";
-import MeetingRoute from "./meetingRoutes"
-import EmailRoute from './EmailRoutes'
-import TaskRoute from "./taskRoutes"
-import UserRoute from "./userRoutes"
-import PolicyRoute from "./policyRoutes"
-import DocumentRoute from './documentRoutes'
-import PolicyDocumentRoute from './policyDocumentRoutes'
-import emailTemmplateRoute from './emailTemplateRoutes'
+import LeadRoute from "./leadRoutes.js";
+import ContactRoute from "./contactRoutes.js";
+import ClaimRoute from "./claimRoutes.js";
+import NoteRoute from "./noteRoutes.js";
+import CallRoute from "./callRoutes.js";
+import MeetingRoute from "./meetingRoutes.js";
+import EmailRoute from "./EmailRoutes.js";
+import TaskRoute from "./taskRoutes.js";
+import UserRoute from "./userRoutes.js";
+import PolicyRoute from "./policyRoutes.js";
+import DocumentRoute from "./documentRoutes.js";
+import PolicyDocumentRoute from "./policyDocumentRoutes.js";
+import EmailTemplateRoute from "./emailTemplateRoutes.js";
 
-router.use('/lead', LeadRoute);
-router.use('/contact', ContactRoute);
-router.use('/claim', ClaimRoute);
-router.use('/note', NoteRoute);
-router.use('/call', CallRoute)
-router.use('/meeting', MeetingRoute)
-router.use('/email', EmailRoute)
-router.use('/task', TaskRoute)
-router.use('/user', UserRoute)
-router.use('/policy', PolicyRoute)
-router.use('/document', DocumentRoute)
-router.use('/policydocument', PolicyDocumentRoute)
-router.use('/emailtemplate', emailTemmplateRoute)
+router.use("/lead", LeadRoute);
+router.use("/contact", ContactRoute);
+router.use("/claim", ClaimRoute);
+router.use("/note", NoteRoute);
+router.use("/call", CallRoute);
+router.use("/meeting", MeetingRoute);
+router.use("/email", EmailRoute);
+router.use("/task", TaskRoute);
+router.use("/user", UserRoute);
+router.use("/policy", PolicyRoute);
+router.use("/document", DocumentRoute);
+router.use("/policydocument", PolicyDocumentRoute);
+router.use("/emailtemplate", EmailTemplateRoute);
 
 export default router;

6740979352d9075003b54a1c25bba8f7e736e3ff
idoneaseg <geral@centronegocios.pt>
2025-10-10 22:44:10 +0100

    Update contactRoutes.js
    fix: add .js extensions to contactRoutes imports
    
    
    Signed-off-by: idoneaseg <geral@centronegocios.pt>

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import Contact from '../controllers/contact'
-import auth from '../middlewares/auth';
+import { Router } from "express";
+import Contact from "../controllers/contact.js";
+import auth from "../middlewares/auth.js";
+
 const router = Router();
 
-router.get('/list', auth,Contact.index)
-router.post('/add', auth,Contact.add)
-router.put('/edit/:id', auth,Contact.edit)
-router.get('/view/:id', auth,Contact.view)
-router.delete('/delete/:id', auth,Contact.deleteData)
-router.post('/deletemany', auth,Contact.deleteMany)
+router.get("/list", auth, Contact.index);
+router.post("/add", auth, Contact.add);
+router.put("/edit/:id", auth, Contact.edit);
+router.get("/view/:id", auth, Contact.view);
+router.delete("/delete/:id", auth, Contact.deleteData);
+router.post("/deletemany", auth, Contact.deleteMany);
 
-
-export default router
+export default router;

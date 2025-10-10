34c9cd9b71f50a0399d5f93f80417a3e6bfdbe87
idoneaseg <geral@centronegocios.pt>
2025-10-10 22:41:12 +0100

    Update leadRoutes.js
    fix: correct leads import and add missing .js extensions
    
    
    Signed-off-by: idoneaseg <geral@centronegocios.pt>

--- server/routes/leadRoutes.js
+++ server/routes/leadRoutes.js
@@ -1,16 +1,15 @@
-import { Router } from 'express';
-import Lead from '../controllers/leads'
-import auth from '../middlewares/auth'
-import { upload } from '../utils/upload'
+import { Router } from "express";
+import Lead from "../controllers/leads.js";
+import auth from "../middlewares/auth.js";
+import { upload } from "../utils/upload.js";
 
 const router = Router();
 
-router.get('/list', auth, Lead.index)
-router.post('/add', auth, Lead.add)
-router.put('/edit/:id', auth, Lead.edit)
-router.get('/view/:id', auth, Lead.view)
-router.delete('/delete/:id', auth, Lead.deleteData)
-router.post('/deletemany', auth, Lead.deleteMany)
+router.get("/list", auth, Lead.index);
+router.post("/add", auth, Lead.add);
+router.put("/edit/:id", auth, Lead.edit);
+router.get("/view/:id", auth, Lead.view);
+router.delete("/delete/:id", auth, Lead.deleteData);
+router.post("/deletemany", auth, Lead.deleteMany);
 
-
-export default router
+export default router;

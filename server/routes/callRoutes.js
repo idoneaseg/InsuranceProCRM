1b0441805f8261c5c3fa1579ffbd94ac97a33aa4
idoneaseg <geral@centronegocios.pt>
2025-10-11 19:31:37 +0100

    Update callRoutes.js
    fix: corrected call controller import path and added .js extensions
    
    
    Signed-off-by: idoneaseg <geral@centronegocios.pt>

--- server/routes/callRoutes.js
+++ server/routes/callRoutes.js
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import Calls from '../controllers/calls';
-import auth from '../middlewares/auth'
+import { Router } from "express";
+import Call from "../controllers/callController.js";
+import auth from "../middlewares/auth.js";
+
 const router = Router();
 
-router.get('/list',auth, Calls.index)
-router.post('/add', auth,Calls.add)
-router.get('/view/:id', auth,Calls.view)
-router.put('/edit/:id', auth,Calls.edit)
-router.delete('/delete/:id', auth,Calls.deleteData)
-router.post('/deletemany', auth,Calls.deleteMany)
+router.get("/list", auth, Call.index);
+router.post("/add", auth, Call.add);
+router.get("/view/:id", auth, Call.view);
+router.put("/edit/:id", auth, Call.edit);
+router.delete("/delete/:id", auth, Call.deleteData);
+router.post("/deletemany", auth, Call.deleteMany);
 
-
-export default router
+export default router;

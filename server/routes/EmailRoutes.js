facbf2a9a5f5a05a643fa19718d3b7dcb41566bc
idoneaseg <geral@centronegocios.pt>
2025-10-11 19:45:11 +0100

    Update EmailRoutes.js
    fix: add .js extensions to email route imports
    
    
    Signed-off-by: idoneaseg <geral@centronegocios.pt>

--- server/routes/EmailRoutes.js
+++ server/routes/EmailRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import Email from '../controllers/email'
-import auth from '../middlewares/auth'
+import Email from '../controllers/email.js';
+import auth from '../middlewares/auth.js';
+
 const router = Router();
 
-router.get('/list', Email.index)
-router.post('/add', Email.add)
-router.get('/view/:id', Email.view)
-router.delete('/delete/:id', Email.deleteData)
-router.post('/deletemany', Email.deleteMany)
+router.get('/list', auth, Email.index);
+router.post('/add', auth, Email.add);
+router.get('/view/:id', auth, Email.view);
+router.delete('/delete/:id', auth, Email.deleteData);
+router.post('/deletemany', auth, Email.deleteMany);
 
-
-export default router
+export default router;

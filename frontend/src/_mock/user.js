eecd12daeebbe7f4247b265bf912d3993eb15ea2
idoneaseg <geral@centronegocios.pt>
2025-10-13 21:33:13 +0000

    fix: update faker methods in user.js for v10 compatibility

--- frontend/src/_mock/user.js
+++ frontend/src/_mock/user.js
@@ -1,27 +1,15 @@
 import { faker } from '@faker-js/faker';
-import { sample } from 'lodash';
 
 // ----------------------------------------------------------------------
 
-const users = [...Array(24)].map((_, index) => ({
-  id: faker.datatype.uuid(),
-  avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
-  name: faker.name.fullName(),
+const users = [...Array(24)].map(() => ({
+  id: faker.string.uuid(),
+  avatarUrl: `/assets/images/avatars/avatar_${faker.number.int({ min: 1, max: 25 })}.jpg`,
+  name: faker.person.fullName(),
   company: faker.company.name(),
   isVerified: faker.datatype.boolean(),
-  status: sample(['active', 'banned']),
-  role: sample([
-    'Leader',
-    'Hr Manager',
-    'UI Designer',
-    'UX Designer',
-    'UI/UX Designer',
-    'Project Manager',
-    'Backend Developer',
-    'Full Stack Designer',
-    'Front End Developer',
-    'Full Stack Developer',
-  ]),
+  status: faker.helpers.arrayElement(['active', 'banned']),
+  role: faker.person.jobTitle(),
 }));
 
 export default users;
